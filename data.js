6c51098f7eafd05fe27dc008b3988c59049cc270
Charlie Xu <xucharliey@gmail.com>
2016-07-11 14:41:57 -0400

    Added more VR stuff

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dorm",
+      "name": "dorm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 0.01878139089539488,
+        "pitch": 0,
+        "fov": 1.7316886462579433
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
