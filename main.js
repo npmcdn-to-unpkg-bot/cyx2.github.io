3bf78dcaca2f380a1f68b176277883874a0b064a
Charlie Xu <xucharliey@gmail.com>
2016-07-13 14:13:42 -0400

    Started customizing home view

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-// Home is initially hidden
+
 $( document ).ready(function() {
-    console.log( "ready!" );
-    $(".home").fadeTo('slow', 1);
-    console.log( "faded in!" );
+	// Fade animation for first view
+    $('.homeView').fadeTo('slow', 1);
+
+    $('#homeButton').click(function() {
+    	$('.navButton').removeClass('active');
+    	$('#homeButton').addClass('active');
+    	$('.views').hide();
+    	$('.homeView').fadeTo('slow', 1);
+    });
+    $('#photographyButton').click(function() {
+    	$('.navButton').removeClass('active');
+    	$('#photographyButton').addClass('active');
+    	$('.views').hide();
+    	$('.photographyView').fadeTo('slow', 1);
+    });
+    $('#aboutButton').click(function() {
+    	$('.navButton').removeClass('active');
+    	$('#aboutButton').addClass('active');
+    	$('.views').hide();
+    	$('.aboutView').fadeTo('slow', 1);
+    });
 });
