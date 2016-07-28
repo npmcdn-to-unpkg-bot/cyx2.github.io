4789869c0aa73766af437596b5e54627ad4c7243
Charlie Xu <xucharliey@gmail.com>
2016-07-28 00:35:22 -0400

    Switched site to simple design

--- main.js
+++ main.js
@@ -1,35 +1,10 @@
 
 $( document ).ready(function() {
-	// Fade animation for first view
-    $('.homeView').fadeTo('slow', 1);
-
-    $('#homeButton').click(function() {
-    	$('.navButton').removeClass('active');
-    	$('#homeButton').addClass('active');
-        $('.clearfix').addClass('masthead');
-    	$('.viewSelector').hide();
-    	$('.homeView').fadeTo('slow', 1);
-    });
-    $('#photographyButton').click(function() {
-    	$('.navButton').removeClass('active');
-    	$('#photographyButton').addClass('active');
-        $('.clearfix').removeClass('masthead');
-    	$('.viewSelector').hide();
-    	$('.photographyView').fadeTo('slow', 1);
-    	var $grid = $('.grid').imagesLoaded( function() {
-		  $grid.masonry({
-		    itemSelector: '.grid-item',
-		    percentPosition: true,
-		    columnWidth: '.grid-sizer'
-		  }); 
-		});
-    });
-    $('#aboutButton').click(function() {
-    	$('.navButton').removeClass('active');
-    	$('#aboutButton').addClass('active');
-        $('.clearfix').addClass('masthead');
-    	$('.viewSelector').hide();
-    	$('.aboutView').fadeTo('slow', 1);
-    });
-
+	var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    }); 
+  });
 });
